96416f9e31a9a04c943448882ca68a7187afcf54
donjohnsondd682 <36606776+donjohnsondd682@users.noreply.github.com>
2018-02-18 17:09:24 -0500

    Add files via upload

---
+++ EW.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace EW
+{
+    partial class TestHarness
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.send = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(19, 15);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(142, 29);
+            this.send.TabIndex = 0;
+            this.send.Text = "Send Data To LM";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(19, 51);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(253, 199);
+            this.textBox.TabIndex = 1;
+            // 
+            // TestHarness
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.send);
+            this.Name = "TestHarness";
+            this.Text = "EWTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
